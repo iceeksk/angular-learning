ce3e1d24e63d1ecc48421d0ee1b8816e36a7daf4
Iceeksk <iceeksk@gmail.com>
2017-03-22 00:38:44 +0200

    create angular-add-dell-tasks-with-checkboxes

--- angular-add-dell-tasks-with-checkboxes/js/controller.js
+++ angular-add-dell-tasks-with-checkboxes/js/controller.js
@@ -1,1 +1,37 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myTasks', ['$scope', '$http', function ($scope, $http) {
+    $scope.h1 = "My Tasks";
+    $scope.h3 = "Total of tasks: ";
+
+    $http.get('data/model.json').success(function (data) {
+        $scope.tasks = data;
+        $scope.totalOfTasks = function () {
+            return $scope.tasks.length;
+        };
+    });
+
+
+
+    $scope.addTask = function () {
+        if($scope.newTask) {
+            $scope.tasks.push({'name': $scope.newTask, 'status': false});
+            $scope.newTask = '';
+        }
+    };
+
+    $scope.clear = function () {
+        $scope.tasks = $scope.tasks.filter(function (item) {
+            return item.status === false;
+        })
+    };
+
+    /*$scope.clear = function () {
+        for(var i = 0; i < $scope.tasks.length; i++){
+            if($scope.tasks[i].status === true){
+                $scope.tasks.splice(i,1);
+                i--;
+            }
+        }
+     };*/
+}]);
