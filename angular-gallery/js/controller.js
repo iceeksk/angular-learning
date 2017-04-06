85d1e2ec5156263d5f3780cc2d2c6b17f5edab8c
Sihnevich Kostantin <kosi@qapint.com>
2017-04-06 19:22:44 +0300

    correct(angular-gallery): made directives

--- angular-gallery/js/controller.js
+++ angular-gallery/js/controller.js
@@ -1,19 +1,17 @@
 var galleryApp = angular.module('galleryApp', ['packery-angular']);
 
-galleryApp.directive('customOnChange', function () {
-	return {
-		restrict: 'A',
-		link: function (scope, element, attrs) {
-			var onChangeHandler = scope.$eval(attrs.customOnChange);
-			element.bind('change', onChangeHandler);
-		}
-	};
-});
 
 galleryApp.controller('galleryCtrl', ['$scope', '$http', function ($scope, $http) {
 
 	$http.get('data/model.json').success(function (data) {
 		$scope.images = data;
+	});
+
+	$scope.showInput = false;
+
+	$scope.$on('someEvent', function() {
+		$scope.showInput = true;
+		console.log('2');
 	});
 
 	$scope.options = {
@@ -30,6 +28,13 @@ galleryApp.controller('galleryCtrl', ['$scope', '$http', function ($scope, $http
 
 	$scope.showPopup = false;
 
+	$scope.openPopup = function (index) {
+		$scope.indexOfImg = index;
+		$scope.showPopup = true;
+	};
+
+	sessionStorage.clear();
+
 	$scope.uploadImg = function (event) {
 		var file = event.target.files,
 			myImg,
@@ -38,13 +43,7 @@ galleryApp.controller('galleryCtrl', ['$scope', '$http', function ($scope, $http
 			size,
 			reader = new FileReader();
 
-		if (random < 0.5) {
-			size = 'normal';
-		} else if (random > 0.75) {
-			size = 'portret';
-		} else {
-			size = 'landscape';
-		}
+		random < 0.5 ? size = 'normal' : random > 0.75 ? size = 'portret' : size = 'landscape' ;
 
 		reader.readAsDataURL(inputImg.files[0]);
 
@@ -61,99 +60,6 @@ galleryApp.controller('galleryCtrl', ['$scope', '$http', function ($scope, $http
 				});
 			$scope.$apply();
 		};
-	};
-
-	$scope.openPopup = function (index) {
-		$scope.indexOfImg = index;
-		$scope.showPopup = true;
-	};
-
-	$scope.closePopup = function () {
-		$scope.showPopup = false;
-	};
-
-	$scope.sendComment = function () {
-
-		var date = new Date();
-
-		if( $scope.nickName && $scope.newComment ){
-			$scope.images[$scope.indexOfImg].comments.push(
-				{
-					"name": "By " + $scope.nickName,
-					"time": date,
-					"comemnt": $scope.newComment
-				}
-			);
-			$scope.nickName = "";
-			$scope.newComment = "";
-		}
-	};
-
-
-	sessionStorage.clear();
-
-	$scope.likeIsTapet = [];
-	$scope.dislikeIsTapet = [];
-
-	$scope.addLike = function () {
-
-		createSessionStorageItemsForLikesAndDislikes();
-
-		var canTapLike = sessionStorage.getItem('canTapLike'+$scope.indexOfImg),
-			canTapDislike = sessionStorage.getItem('canTapDislike'+$scope.indexOfImg);
-
-		if (canTapLike == 'can') {
-			$scope.images[$scope.indexOfImg].like++;
-			sessionStorage.setItem('canTapLike'+$scope.indexOfImg, 'no');
-			if (canTapDislike == 'no') {
-				$scope.images[$scope.indexOfImg].dislike--;
-				sessionStorage.setItem('canTapDislike'+$scope.indexOfImg, 'can');
-			}
-		} else if (canTapLike == 'no') {
-			$scope.images[$scope.indexOfImg].like--;
-			sessionStorage.setItem('canTapLike'+$scope.indexOfImg, 'can');
-		}
-
-		$scope.likeIsTapet[$scope.indexOfImg] = sessionStorage.getItem('canTapLike' + $scope.indexOfImg);
-		$scope.dislikeIsTapet[$scope.indexOfImg] = sessionStorage.getItem('canTapDislike' + $scope.indexOfImg);
 
 	};
-
-	$scope.addDislike = function () {
-
-		createSessionStorageItemsForLikesAndDislikes();
-
-		var canTapLike = sessionStorage.getItem('canTapLike' + $scope.indexOfImg),
-			canTapDislike = sessionStorage.getItem('canTapDislike' + $scope.indexOfImg);
-
-		if (canTapDislike == 'can') {
-			$scope.images[$scope.indexOfImg].dislike++;
-			sessionStorage.setItem('canTapDislike' + $scope.indexOfImg, 'no');
-			if (canTapLike == 'no') {
-				$scope.images[$scope.indexOfImg].like--;
-				sessionStorage.setItem('canTapLike' + $scope.indexOfImg, 'can');
-			}
-		} else if (canTapDislike == 'no') {
-			$scope.images[$scope.indexOfImg].dislike--;
-			sessionStorage.setItem('canTapDislike' + $scope.indexOfImg, 'can');
-		}
-
-		$scope.likeIsTapet[$scope.indexOfImg] = sessionStorage.getItem('canTapLike' + $scope.indexOfImg);
-		$scope.dislikeIsTapet[$scope.indexOfImg] = sessionStorage.getItem('canTapDislike' + $scope.indexOfImg);
-
-	};
-
-	function createSessionStorageItemsForLikesAndDislikes() {
-
-		var canTapLike = sessionStorage.getItem('canTapLike' + $scope.indexOfImg),
-			canTapDislike = sessionStorage.getItem('canTapDislike' + $scope.indexOfImg);
-
-		if (canTapLike == null) {
-			sessionStorage.setItem('canTapLike' + $scope.indexOfImg, 'can');
-		}
-		if (canTapDislike == null) {
-			sessionStorage.setItem('canTapDislike' + $scope.indexOfImg, 'can');
-		}
-	}
-
 }]);
---
+++ angular-gallery/js/galeryItem.directive.js
@@ -0,0 +1,21 @@
+galleryApp.directive("galleryItem", function () {
+	return {
+		restrict: "E",
+		replace: true,
+		scope: {
+			customImage: '='
+		},
+		templateUrl: "templates/gallery-item.html",
+		compile: function(element, attributes){
+
+			return {
+				pre: function(scope, element, attributes, controller, transcludeFn){
+
+				},
+				post: function(scope, element, attributes, controller, transcludeFn){
+					scope.$emit('someEvent');
+				}
+			}
+		},
+	}
+});
